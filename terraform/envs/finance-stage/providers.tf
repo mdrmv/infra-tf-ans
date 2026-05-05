175c15566969837e5f1f6a9bf357a4766c89e754
marat <madraimovm@gmail.com>
2026-05-05 14:23:17 +0500

    fix: add terraform / ansible states: change storage_endpoint to KZ

--- terraform/envs/finance-stage/providers.tf
+++ terraform/envs/finance-stage/providers.tf
@@ -3,5 +3,6 @@ provider "yandex" {
   folder_id                = var.folder_id
   zone                     = var.zone
   endpoint                 = var.yandex_api_endpoint
+  storage_endpoint         = var.yandex_storage_endpoint
   service_account_key_file = pathexpand(var.service_account_key_file)
 }
